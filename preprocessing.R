26fd7cefa26a0edc553d31ba53353d0dc597d4f7
Valentin Hilffert <vahilffe@gmail.com>
2019-02-20 14:52:08 +0100

    fix that several countries are displayed fix that numbers instead of countries are displayed
    when geographic coverage = "continental" a number instead of the respective countries were displayed. stringsAsFactors = FALSE solved it
    
    in the sidebar panel countries were seen twice due to an additional whitespace before the country's name.

--- preprocessing.R
+++ preprocessing.R
@@ -3,7 +3,7 @@ source("countries.R")
 
 # Download the data from the online-sheet and load
 drive_download(as_id("1CXGo4Zh8i7R4aLlCmdqKbNr9qH5xeH9Lq2h3-UMqWh4"),type = "csv",overwrite = TRUE)
-tb_sdg<-read.csv("Test for IndicatorDB (Antworten).csv")
+tb_sdg<-read.csv("Test for IndicatorDB (Antworten).csv", stringsAsFactors = FALSE)
 
 # Replace NAs by empty values
 tb_sdg[is.na(tb_sdg)] <- ""
@@ -23,16 +23,21 @@ tb_sdg2 <- tb_sdg %>% # filling countries if only global or continent
                                paste(geo_tags$country, collapse = ","), 
                                country_init),
          country_init = ifelse(`Geographic.coverage` == "Continental",
-                               paste(geo_tags$country[geo_tags$continent == "Asia"], 
+                               paste(geo_tags$country[geo_tags$continent %in% strsplit(tb_sdg$`Which.continents.are.covered.`, ", ")], 
                                      collapse = ","),
-                               country_init)) %>% # Separate contry
-  separate(`Which.countries.are.covered..1`, country_vars, sep = ",", remove = FALSE) %>%
+                               country_init))  %>% 
+  separate(country_init, country_vars, sep = ",", remove = TRUE) %>%
   gather(to_delete, country, -(to_keep)) %>% # Long form
   select(-to_delete) %>% # delete empty lines
   filter(country != "" & !is.na(country))
+
+# trim country to assure unique values
+tb_sdg2$country <- trimws(tb_sdg2$country)
+sort(unique(tb_sdg2$country))
+
+
 
 # convert start-date
 tb_sdg2$Start[tb_sdg2$Start=="before 1990"]<-"1990"
 tb_sdg2$Start<-as.numeric(as.character(tb_sdg2$Start))
 # what to do with undefined in this column?
-
